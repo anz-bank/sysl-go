53460695ac9d1bb749efc0c31e6706f9cb31a438
Andrew Emery <andrew.emery@anz.com>
2020-03-30 11:07:07 +1100

    Add build metadata
    This change adds the ability to set build metadata at build time. Build
    metadata is regarded as data that is common to all server builds (name,
    version, etc.).
    
    Closes: #4

---
+++ core/build_metadata.go
@@ -0,0 +1,40 @@
+package core
+
+import "github.com/anz-bank/sysl-go/status"
+
+var (
+	// Build metadata. These values are intended to be overridden by values
+	// supplied by the build environment at link time. Values are specified
+	// by passing "-X package.Varname=Value" to the linker. For example:
+	//
+	//  go build -ldflags="-X main.Name=TestApp -X main.Version=1.0" ...etc...
+	//
+	// Ref: https://golang.org/cmd/link/
+
+	// Name is set at the build time
+	Name = ""
+
+	// Version is set at the build time
+	Version = ""
+
+	// BuildID is set at the build time
+	BuildID = ""
+
+	// CommitSha is set at the build time
+	CommitSha = ""
+
+	// BranchName is set at the build time
+	BranchName = ""
+
+	// TagName is set at the build time
+	TagName = ""
+)
+
+var buildMetadata = &status.BuildMetadata{
+	Name:       Name,
+	Version:    Version,
+	BuildID:    BuildID,
+	CommitSha:  CommitSha,
+	BranchName: BranchName,
+	TagName:    TagName,
+}
