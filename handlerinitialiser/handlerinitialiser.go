340b157a77cd8045aa1770775ff0c6194e6ea4f4
Sumit Narayan <sumit.narayan@anz.com>
2020-05-01 11:20:31 +1000

    Fix router transform.

--- handlerinitialiser/handlerinitialiser.go
+++ handlerinitialiser/handlerinitialiser.go
@@ -9,7 +9,7 @@ import (
 
 type HandlerInitialiser interface {
 	WireRoutes(ctx context.Context, r chi.Router)
-	Name() string                // Human-friendly name of the service
+	Name() string        // Human-friendly name of the service
 	Config() interface{} // Reference to config for this service.
 }
 
