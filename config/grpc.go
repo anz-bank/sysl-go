4878f4ad2ab9c74d0c1b165b91c579479c29692f
Marcelo Cantos <marcelo.cantos@anz.com>
2020-07-10 09:57:11 +1000

    gRPC downstream injection

--- config/grpc.go
+++ config/grpc.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"log"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
@@ -18,4 +20,43 @@ func ExtractGrpcServerOptions(cfg *CommonServerConfig) ([]grpc.ServerOption, err
 	creds := credentials.NewTLS(tlsConfig)
 
 	return []grpc.ServerOption{grpc.Creds(creds)}, nil
+}
+
+// CommonGRPCDownstreamData collects all the client gRPC configuration
+type CommonGRPCDownstreamData struct {
+	ServiceAddress string     `yaml:"serviceAddress"`
+	TLS            *TLSConfig `yaml:"tls"`
+	WithBlock      bool       `yaml:"withBlock"`
+}
+
+func NewDefaultCommonGRPCDownstreamData() *CommonGRPCDownstreamData {
+	return &CommonGRPCDownstreamData{}
+}
+
+// DefaultGRPCClient returns a new *grpc.ClientConn with sensible defaults, in
+// particular it has a timeout set!
+func DefaultGRPCClient(cfg *CommonGRPCDownstreamData) (*grpc.ClientConn, error) {
+	if cfg == nil {
+		cfg = NewDefaultCommonGRPCDownstreamData()
+	}
+
+	var opts []grpc.DialOption
+	if cfg.TLS != nil {
+		tlsConfig, err := makeSelfSignedTLSConfig(cfg.TLS)
+		if err != nil {
+			return nil, err
+		}
+		creds := credentials.NewTLS(tlsConfig)
+		if err != nil {
+			log.Fatalf("Failed to create TLS credentials %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
+
+	if cfg.WithBlock {
+		opts = append(opts, grpc.WithBlock())
+	}
+	return grpc.Dial(cfg.ServiceAddress, opts...)
 }
