00addfac9a4475f7534a56b7e0795382efa39b77
Marcelo Cantos <marcelo.cantos@anz.com>
2020-07-28 13:30:54 +1000

    Cleanup linter diagnostics
    Mostly disable a bunch of new linters

--- core/server.go
+++ core/server.go
@@ -30,7 +30,6 @@ func (e *emptyWriter) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
-//nolint:gocognit // Long method names are okay because only generated code will call this, not humans.
 func NewServerParams(ctx context.Context, name string, opts ...ServerOption) *ServerParams {
 	params := &ServerParams{Ctx: ctx, Name: name}
 	for _, o := range opts {
