6b05cf7e1b515edf48e18baf82c05338196616e9
fletcher <reuben.fletcher-costin@anz.com>
2020-09-24 10:08:43 +1000

    add autogen smoke test for REST error type with field named Error
    > Types that have an Error field and are used as an error type
    > (eg: rest API response body for 4xx or 5xx error codes) fail
    > to compile. This is due to a clash between the Error field
    > in the generated type, and the Error() string method that
    > makes that type an error.
    
    Ref: https://github.com/anzx/sysl-go/issues/31

---
+++ codegen/arrai/auto/tests/rest_error_downstream/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	gateway "rest_error_downstream/gen/pkg/servers/Gateway"
+	backend "rest_error_downstream/gen/pkg/servers/Gateway/backend"
+
+	"github.com/anz-bank/pkg/log"
+	"github.com/anz-bank/sysl-go/common"
+	"github.com/anz-bank/sysl-go/core"
+)
+
+type AppConfig struct{}
+
+// FIXME why does autogen tack a "List" suffix on the name of this?
+func GetApiDoopList(ctx context.Context, req *gateway.GetApiDoopListRequest, client gateway.GetApiDoopListClient) (*gateway.GatewayResponse, error) {
+	err := client.BackendPostDoop(ctx, &backend.PostDoopRequest{})
+	if err != nil {
+		downstreamErr, ok := err.(*common.DownstreamError)
+		if ok {
+			ter, ok := downstreamErr.Cause.(*backend.ErrorResponse)
+			if ok {
+				// Note: sysl-go autogen secretly name-mangles the field with a "_" suffix
+				// to disambiguate it from the Error() method.
+				msg := fmt.Sprintf("backend sent us an ErrorResponse: %s", ter.Error_)
+				return &gateway.GatewayResponse{Content: msg}, nil
+			}
+		}
+		return nil, err
+	}
+	return &gateway.GatewayResponse{Content: "???"}, nil
+}
+
+func application(ctx context.Context) error {
+	return gateway.Serve(ctx,
+		func(ctx context.Context, config AppConfig) (*gateway.ServiceInterface, *core.RestCallback, error) {
+
+			// FIXME auto codegen and common.MapError don't align.
+			mapError := func(ctx context.Context, err error) *common.HTTPError {
+				httpErr := common.MapError(ctx, err)
+				return &httpErr
+			}
+
+			return &gateway.ServiceInterface{
+					GetApiDoopList: GetApiDoopList,
+				}, &core.RestCallback{
+					MapError: mapError,
+				},
+				nil
+		},
+	)
+}
+
+func main() {
+	// initialise context with pkg logger
+	logger := log.NewStandardLogger()
+	ctx := log.WithLogger(logger).Onto(context.Background())
+
+	err := application(ctx)
+
+	if err != nil {
+		log.Error(ctx, err)
+		os.Exit(1)
+	}
+}
