845b5fe7be65daa96c747889578571d13216b0cb
fletcher <reuben.fletcher-costin@anz.com>
2020-10-20 10:13:52 +1100

    delete deprecated codegen approaches
    - delete sysl transforms codegen
    - delete first-generation arrai codegen

--- codegen/arrai/vendorme.go
+++
@@ -1,1 +0,0 @@
-package arrai
--- codegen/testdata/grpc_downstream/Module.mk
+++
@@ -1,5 +0,0 @@
-codegen/tests/grpc_downstream/grpc_downstream.pb.go: PROTO_IN=$(TEST_IN_DIR)/grpc_downstream
-codegen/tests/grpc_downstream/grpc_downstream.pb.go: PROTO_OUT=$(TEST_OUT_DIR)/grpc_downstream
-
-codegen/tests/grpc_downstream/grpc_downstream.pb.go : $(TEST_IN_DIR)/grpc_downstream/grpc_downstream.proto
-	$(run-protoc)
