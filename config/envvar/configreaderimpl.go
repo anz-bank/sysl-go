065c5d389b4a6b87f4a6e17290c2816722928c15
fletcher <reuben.fletcher-costin@anz.com>
2020-11-16 13:31:04 +1100

    refactor to reduce func length, appease linter

--- config/envvar/configreaderimpl.go
+++ config/envvar/configreaderimpl.go
@@ -68,39 +68,7 @@ func (m configReaderImpl) Unmarshal(config interface{}) error {
 		})
 	}
 
-	decodeHook := viper.DecodeHook(
-		mapstructure.ComposeDecodeHookFunc(
-			// Function to accommodate for log level.
-			func(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
-				if f.Kind() != reflect.String {
-					return data, nil
-				}
-				switch strings.ToLower(data.(string)) {
-				case "panic":
-					return logrus.PanicLevel, nil
-				case "fatal":
-					return logrus.FatalLevel, nil
-				case "error":
-					return logrus.ErrorLevel, nil
-				case "warn":
-					return logrus.WarnLevel, nil
-				case "info":
-					return logrus.InfoLevel, nil
-				case "debug":
-					return logrus.DebugLevel, nil
-				case "trace":
-					return logrus.TraceLevel, nil
-				default:
-					return data, nil
-				}
-			},
-			// Function to support jsontime.Duration
-			jsontime.DurationMapstructureDecodeHookFunc,
-			// Appended by the two default functions
-			mapstructure.StringToTimeDurationHookFunc(),
-			mapstructure.StringToSliceHookFunc(","),
-		),
-	)
+	decodeHook := viper.DecodeHook(makeDefaultDecodeHook())
 
 	opts = append(opts, decodeHook)
 
@@ -109,34 +77,76 @@ func (m configReaderImpl) Unmarshal(config interface{}) error {
 	}
 
 	if m.strictMode {
-		// Filter away any unused keys that should be ignored.
-		// Beware: for nested keys, mapstructure will not
-		// necessarily report the full key in metadata.Unused:
-		// For example, if we unmarshal into a config structure
-		// with no "fizz" key, and there is a nested key named
-		// "fizz.buzz" in the input, then mapstructure will report
-		// the name of the unused key as "fizz". However, if there
-		// is a "fizz" key in the config structure, but no
-		// "buzz" key in that "fizz" structure, then mapstructure
-		// will report the name of the unused key as "fizz.buzz".
-		toIgnore := make(map[string]struct{})
-		for _, k := range m.strictModeIgnoredKeys {
-			k = strings.ToLower(k)
-			toIgnore[k] = struct{}{}
-		}
-		unusedNotIgnored := make([]string, 0)
-		for _, unusedKey := range metadata.Unused {
-			_, ok := toIgnore[unusedKey]
-			if ok {
-				continue
-			}
-			unusedNotIgnored = append(unusedNotIgnored, unusedKey)
-		}
-		if len(unusedNotIgnored) > 0 {
-			msg := fmt.Sprintf("Misconfiguration error: found unexpected config key(s): %s", strings.Join(unusedNotIgnored, ","))
-			return fmt.Errorf(msg)
+		err := m.validateNoUnusedKeys(metadata)
+		if err != nil {
+			return err
 		}
 	}
 
 	return nil
+}
+
+func (m configReaderImpl) validateNoUnusedKeys(metadata *mapstructure.Metadata) error {
+	// Filter away any unused keys that should be ignored.
+	// Beware: for nested keys, mapstructure will not
+	// necessarily report the full key in metadata.Unused:
+	// For example, if we unmarshal into a config structure
+	// with no "fizz" key, and there is a nested key named
+	// "fizz.buzz" in the input, then mapstructure will report
+	// the name of the unused key as "fizz". However, if there
+	// is a "fizz" key in the config structure, but no
+	// "buzz" key in that "fizz" structure, then mapstructure
+	// will report the name of the unused key as "fizz.buzz".
+	toIgnore := make(map[string]struct{})
+	for _, k := range m.strictModeIgnoredKeys {
+		k = strings.ToLower(k)
+		toIgnore[k] = struct{}{}
+	}
+	unusedNotIgnored := make([]string, 0)
+	for _, unusedKey := range metadata.Unused {
+		_, ok := toIgnore[unusedKey]
+		if ok {
+			continue
+		}
+		unusedNotIgnored = append(unusedNotIgnored, unusedKey)
+	}
+	if len(unusedNotIgnored) > 0 {
+		msg := fmt.Sprintf("Misconfiguration error: found unexpected config key(s): %s", strings.Join(unusedNotIgnored, ","))
+		return fmt.Errorf(msg)
+	}
+	return nil
+}
+
+func makeDefaultDecodeHook() mapstructure.DecodeHookFunc {
+	return mapstructure.ComposeDecodeHookFunc(
+		// Function to accommodate for log level.
+		func(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
+			if f.Kind() != reflect.String {
+				return data, nil
+			}
+			switch strings.ToLower(data.(string)) {
+			case "panic":
+				return logrus.PanicLevel, nil
+			case "fatal":
+				return logrus.FatalLevel, nil
+			case "error":
+				return logrus.ErrorLevel, nil
+			case "warn":
+				return logrus.WarnLevel, nil
+			case "info":
+				return logrus.InfoLevel, nil
+			case "debug":
+				return logrus.DebugLevel, nil
+			case "trace":
+				return logrus.TraceLevel, nil
+			default:
+				return data, nil
+			}
+		},
+		// Function to support jsontime.Duration
+		jsontime.DurationMapstructureDecodeHookFunc,
+		// Appended by the two default functions
+		mapstructure.StringToTimeDurationHookFunc(),
+		mapstructure.StringToSliceHookFunc(","),
+	)
 }
