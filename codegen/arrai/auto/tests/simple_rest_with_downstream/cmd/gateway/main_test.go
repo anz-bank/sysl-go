6d810290b768de7e39afbd38c7815709f3f2d49b
fletcher <reuben.fletcher-costin@anz.com>
2020-11-17 11:44:34 +1100

    cleanup test simple_rest_with_downstream - stop server

--- codegen/arrai/auto/tests/simple_rest_with_downstream/cmd/gateway/main_test.go
+++ codegen/arrai/auto/tests/simple_rest_with_downstream/cmd/gateway/main_test.go
@@ -169,8 +169,22 @@ func TestSimpleRestWithDownstreamAppSmokeTest(t *testing.T) {
 		require.NoError(t, err)
 	}()
 
-	// Start gateway application running as server
-	go application(ctx)
+	appServer, err := newAppServer(ctx)
+	require.NoError(t, err)
+	defer func() {
+		err := appServer.Stop()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	// Start application server
+	go func() {
+		err := appServer.Start()
+		if err != nil {
+			panic(err)
+		}
+	}()
 
 	// Wait for application to come up
 	backoff, err := retry.NewFibonacci(20 * time.Millisecond)
@@ -195,6 +209,4 @@ func TestSimpleRestWithDownstreamAppSmokeTest(t *testing.T) {
 	actual, err = doGatewayRequestResponse(ctx, "notfound")
 	require.NotNil(t, err)
 	require.Contains(t, err.Error(), "503")
-
-	// FIXME how do we stop the application server?
 }
