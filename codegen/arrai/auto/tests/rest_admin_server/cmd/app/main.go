4b0205f2764f32e45f2445a91e428ae3c3d755bc
fletcher <reuben.fletcher-costin@anz.com>
2020-11-19 14:40:58 +1100

    add smoke test for rest app with admin server

---
+++ codegen/arrai/auto/tests/rest_admin_server/cmd/app/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	app "rest_admin_server/internal/gen/pkg/servers/app"
+
+	"github.com/anz-bank/pkg/log"
+	"github.com/anz-bank/sysl-go/core"
+)
+
+type AppConfig struct{}
+
+func GetHello(ctx context.Context, req *app.GetHelloListRequest) error {
+  return nil
+}
+
+func newAppServer(ctx context.Context) (core.StoppableServer, error) {
+	return app.NewServer(ctx,
+		func(ctx context.Context, config AppConfig) (*app.ServiceInterface, *core.Hooks, error) {
+			return &app.ServiceInterface{
+          GetHelloList: GetHello,
+				}, &core.Hooks{},
+				nil
+		},
+	)
+}
+
+func main() {
+	// initialise context with pkg logger
+	logger := log.NewStandardLogger()
+	ctx := log.WithLogger(logger).WithConfigs(log.SetVerboseMode(true)).Onto(context.Background())
+
+	handleError := func(err error) {
+		if err != nil {
+			log.Error(ctx, err)
+			os.Exit(1)
+		}
+	}
+
+	srv, err := newAppServer(ctx)
+	handleError(err)
+	err = srv.Start()
+	handleError(err)
+}
---
+++ codegen/arrai/auto/tests/rest_admin_server/go.mod
@@ -0,0 +1,16 @@
+module rest_admin_server
+
+go 1.14
+
+require (
+	github.com/anz-bank/pkg v0.0.27
+	github.com/anz-bank/sysl-go v0.84.0
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/sethvargo/go-retry v0.1.0
+	github.com/stretchr/testify v1.6.1
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+)
+
+replace github.com/anz-bank/sysl-go => ../../../../..
