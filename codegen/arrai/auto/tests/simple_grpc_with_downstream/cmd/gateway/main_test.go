e5e54fc9dcb002a29c884d53ef9399ed653c142c
Arieh Schneier <15041913+AriehSchneier@users.noreply.github.com>
2021-10-22 11:23:10 +1100

    Add MockError for gRPC downstreams (#332)

--- codegen/arrai/auto/tests/simple_grpc_with_downstream/cmd/gateway/main_test.go
+++ codegen/arrai/auto/tests/simple_grpc_with_downstream/cmd/gateway/main_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	pb "simple_grpc_with_downstream/internal/gen/pb/gateway"
 	"simple_grpc_with_downstream/internal/gen/pkg/servers/gateway"
 	"simple_grpc_with_downstream/internal/gen/pkg/servers/gateway/encoder_backend"
@@ -31,5 +33,21 @@ func TestSimpleGRPCWithDownstream(t *testing.T) {
 	gatewayTester.Encode().
 		WithRequest(&pb.EncodeRequest{Content: "hello world!", EncoderId: "rot13"}).
 		ExpectResponse(&pb.EncodeResponse{Content: "uryyb jbeyq!"}).
+		Send()
+}
+
+func TestSimpleGRPCWithDownstream_Fail(t *testing.T) {
+	t.Parallel()
+
+	gatewayTester := gateway.NewTestServer(t, context.Background(), createService, "")
+	defer gatewayTester.Close()
+
+	gatewayTester.Mocks.Encoder_backend.Rot13.
+		ExpectRequest(&encoder_backend.EncodingRequest{Content: "hello world"}).
+		MockError(status.Error(codes.Unknown, "Failed!"))
+
+	gatewayTester.Encode().
+		WithRequest(&pb.EncodeRequest{Content: "hello world", EncoderId: "rot13"}).
+		ExpectError(status.Error(codes.Unknown, "Failed!")).
 		Send()
 }
